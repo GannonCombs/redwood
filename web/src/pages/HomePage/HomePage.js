434d6b0592a4d8b3bc17c550aa07c737f58f769d
Gannon Combs <gannoncombs@protonmail.com>
2021-05-22 22:28:03 -0400

    Created page links and layouts.

---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,13 @@
+// web/src/pages/HomePage/HomePage.js
+
+import { Link, routes } from '@redwoodjs/router'
+
+const HomePage = () => {
+  return (
+    <>
+      <main>Home</main>
+    </>
+  )
+}
+
+export default HomePage
