0cf932fd660d64ee69c8157cf5927eb302032edb
Gannon Combs <gannoncombs@protonmail.com>
2021-05-23 15:02:19 -0400

    Add cells and create standalone blog pages.

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,13 +1,9 @@
 // web/src/pages/HomePage/HomePage.js
 
-import { Link, routes } from '@redwoodjs/router'
+import BlogPostsCell from 'src/components/BlogPostsCell'
 
 const HomePage = () => {
-  return (
-    <>
-      <main>Home</main>
-    </>
-  )
+  return <BlogPostsCell />
 }
 
 export default HomePage
